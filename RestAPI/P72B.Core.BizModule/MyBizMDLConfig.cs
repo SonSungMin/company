b4102481dac9b2e8d3de31b363af1ef621a82b5f
SonSungMin <ssm7777777@gmail.com>
2025-12-30 16:48:27 +0900

    Create MyBizMDLConfig.cs

---
+++ RestAPI/P72B.Core.BizModule/MyBizMDLConfig.cs
@@ -0,0 +1,8 @@
+namespace P72B.Core.BizModule.__RemoveMe___
+{
+    internal class MyBizMDLConfig
+    {
+        internal const string _EduMapperName = "HDx.Education.Mapper";
+        internal const string _SalesMapperName = "HDx.Sales.Mapper";
+    }
+}
